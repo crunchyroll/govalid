5de1ee1482e0bf411ddda01539a585c990d164d4
Chris Pennello <git@chrispennello.com>
2015-07-09 20:00:54 -0700

    Initial stab at code parsing.

---
+++ parse.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path"
+
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func validator(name string, s *ast.StructType) {
+	log.Print(name)
+	for _, fld := range(s.Fields.List) {
+		nam := fld.Names[0].Name
+		typ := fld.Type.(*ast.Ident)
+		log.Printf("%s %s", nam, typ)
+	}
+}
+
+func main() {
+	log.SetFlags(0)
+
+	if len(os.Args) != 2 {
+		log.Printf("usage: %s file.v", path.Base(os.Args[0]))
+		os.Exit(2)
+	}
+
+	path := os.Args[1]
+	fs := token.NewFileSet()
+	f, err := parser.ParseFile(fs, path, nil, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, obj := range f.Scope.Objects {
+		if obj.Kind != ast.Typ { continue }
+		ts, ok := obj.Decl.(*ast.TypeSpec)
+		if !ok { continue }
+		s, ok := ts.Type.(*ast.StructType)
+		if !ok { continue }
+		if s.Fields == nil {
+			log.Fatalf("type %s struct has empty field list %v", ts.Name, ts)
+		}
+		validator(ts.Name.Name, s)
+	}
+}
