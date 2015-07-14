ff6e5e01ea1118d5bdf75bb4ace155078859620d
Chris Pennello <git@chrispennello.com>
2015-07-14 10:52:39 -0700

    Adds test valid input file.

---
+++ test.v
@@ -0,0 +1,26 @@
+package foo
+
+type fooTypeSet struct {
+	s string
+
+	bo bool
+
+	f32 float32
+	f64 float64
+
+	i8  int8
+	i16 int16
+	i32 int32
+	i64 int64
+
+	u8  uint8
+	u16 uint16
+	u32 uint32
+	u64 uint64
+}
+
+type blahTypeSet struct {
+	r rune // well-defined?
+	by byte // well-defined?
+
+}
