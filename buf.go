0670bf41fe14ab5b08be7c7c655ec7e705b7532f
Chris Pennello <git@chrispennello.com>
2015-07-14 17:30:01 -0700

    Buffers generated code before any output.
    This is so we can compute whether we need to augment the import list
    ahead of time.

--- buf.go
+++ buf.go
@@ -9,12 +9,13 @@ import (
 
 type buf struct {
 	*bytes.Buffer
+	needsStrconv bool
 }
 
 func newBuf(size int64) *buf {
 	b := make([]byte, 0, size)
 	bu := bytes.NewBuffer(b)
-	return &buf{Buffer: bu}
+	return &buf{Buffer: bu, needsStrconv: false}
 }
 
 func (b *buf) writef(format string, a ...interface{}) {
