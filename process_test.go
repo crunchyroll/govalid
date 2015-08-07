f218a8e7ffaede053829da8b18ce8d8fa475aff9
Chris Pennello <git@chrispennello.com>
2015-08-06 17:16:52 -0700

    Adds a test for process.

---
+++ process_test.go
@@ -0,0 +1,34 @@
+// chris 080615
+
+package main
+
+import (
+	"os"
+	"path"
+	"testing"
+)
+
+func testProcess(t *testing.T, srcname, dstname string) {
+	src, err := os.Open(srcname)
+	if err != nil {
+		t.Error(err)
+	}
+	defer src.Close()
+	flag := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	dst, err := os.OpenFile(dstname, flag, 0666)
+	if err != nil {
+		t.Error(err)
+	}
+	defer dst.Close()
+	err = process(dst, srcname, src)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestProcess(t *testing.T) {
+	srcname := path.Join("test", "comp.v")
+	dstname := path.Join("test", "comp.go")
+	testProcess(t, srcname, dstname)
+	os.Remove(dstname)
+}
